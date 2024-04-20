a510be71a5bfa09f5ce8fd34cbc9de9b662a7304
Harsh Khaiwal <harsh.bt19@nsut.ac.in>
2024-04-20 15:29:56 +0530

    comparison

--- 01-Basics/03_ConversionOperation.js
+++ 01-Basics/03_ConversionOperation.js
@@ -19,3 +19,33 @@ console.log(Boolean(isLoggednIn)); //true
 let someNumber=33
 let stringNumber=String(someNumber);
 console.log(typeof stringNumber); //string
+
+// **********  Conversion Operation **********
+
+let value=33;
+let negValue=-value;
+console.log(negValue);
+
+let str1="Hello";
+let str2="World";
+
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1" + 2 + 3); //123
+console.log(1 + 2 + "3"); //33
+console.log(1 + "2" + 3); //123
+console.log((1 + 2) * 3%3); //0
+
+console.log(+true); //1
+console.log(+""); //0
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 5+6; //not recommended
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
+gameCounter++;
+console.log(gameCounter);
---
+++ 01-Basics/04_Comparison.js
@@ -0,0 +1,17 @@
+// console.log(5>6);//false
+
+console.log("2">1); //true not recommended because it is comparing string with number
+
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0); //true
+
+
+console.log(undefined>0); //false
+console.log(undefined==0); //false
+console.log(undefined>=0); //false
+
+// === => strict equality operator
+
+console.log(1=="1"); //true
+console.log(1==="1"); //false
